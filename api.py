76ce5305a387badef5548210a47f9b6e2e75f5e9
Rohandud <48047872+Rohandud@users.noreply.github.com>
2024-03-15 02:46:02 +0530

    Update api.py

--- api.py
+++ api.py
@@ -30,7 +30,7 @@ def getopenai():
     return json_response
 @app.route('/', methods=['POST'])
 def webhook():
-    update = telebot.types.Update.de_json(flask.request.stream.read().decode('utf-8'))
+    update = telebot.types.Update.de_json(request.stream.read().decode('utf-8'))
     bot.process_new_updates([update])
     return 'ok', 200
 
