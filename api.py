003efcd03d1d0123eb902f6b37ec10be71020ab3
Rohandud <48047872+Rohandud@users.noreply.github.com>
2024-03-15 02:40:11 +0530

    Update api.py

--- api.py
+++ api.py
@@ -5,7 +5,10 @@ from flask import *
 from main1 import openairun
 import datetime
 import json
-
+from py1337x import py1337x
+from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
+bot = telebot.TeleBot('6549292499:AAGmD7uf5R1wgYYgJP8E5yx0okQb7egfQsI', threaded=False)
+torrents = py1337x(proxy='1377x.to', cacheTime=0)
 x = datetime.datetime.now()
 
 # Initializing flask app
@@ -24,7 +27,18 @@ def getopenai():
     json_response = json.dumps(openairesponse)
 
     return json_response
+@app.route('/', methods=['POST'])
+def webhook():
+    update = telebot.types.Update.de_json(flask.request.stream.read().decode('utf-8'))
+    bot.process_new_updates([update])
+    return 'ok', 200
 
+@bot.message_handler(commands=['start'])
+def start(message):
+  user_name = message.from_user.first_name
+  print("start "+user_name)
+  bot.reply_to(message, "Hey "+user_name +"")
+    
 # Running app
 if __name__ == '__main__':
     app.run(debug=True)
